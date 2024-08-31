c217d6e3e0d3753e8d0b1373ea53ab46b2c31995
ijwgt <66661966@bk.ru>
2024-08-31 17:28:28 +0300

    initial

---
+++ src/js/base.js
@@ -0,0 +1,13 @@
+export default class ArrayBufferConverter {
+	constructor() {
+		this.buffer = '';
+	};
+
+	load(buffer) {
+		this.buffer = new Uint16Array(buffer);
+	};
+
+	toString() {
+		return String.fromCharCode.apply(null, this.buffer);
+	};
+}
